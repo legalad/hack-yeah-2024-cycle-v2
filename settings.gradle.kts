750ccb1746705f2425037c6212a0debbd1770a5b
Jakub Nowak <jakub.nowak.eu@gmail.com>
2024-09-28 15:56:21 +0200

    setting up map box dependencies and credentials

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,14 +11,48 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = "mapbox"
+                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
+            }
+        }
     }
 }
 
 rootProject.name = "hack-yeah-2024-cycle-v2"
 include(":app")
+/*
+val keyProps = Properties().apply {
+    file("local.properties").takeIf { it.exists() }?.inputStream()?.use { load(it) }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // below code is added
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = "mapbox"
+                password = keyProps.getProperty("MAPBOX_MAP_TOKEN")
+            }
+        }
+    }
+}*/
  
